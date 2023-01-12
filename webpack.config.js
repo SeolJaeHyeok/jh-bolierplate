4de162fe8cc1409534646f09bd9695cadfadfac5
Jaehyeok <milkboy2564@naver.com>
2023-01-12 18:12:53 +0900

    fix: devServer 웹펙 설정 변경

--- webpack.config.js
+++ webpack.config.js
@@ -1,60 +1,64 @@
-require("dotenv").config();
+require('dotenv').config();
 
-const path = require("path");
-const HtmlWebpackPlugin = require("html-webpack-plugin");
+const path = require('path');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
 
-const isProd = process.env.NODE_ENV === "production";
+const isProd = process.env.NODE_ENV === 'production';
 const PORT = process.env.PORT || 3000;
 
 module.exports = {
-  mode: isProd ? "production" : "development",
-  devtool: isProd ? "hidden-source-map" : "source-map",
-  entry: "./src/index.tsx",
+  mode: isProd ? 'production' : 'development',
+  devtool: isProd ? 'hidden-source-map' : 'source-map',
+  entry: './src/index.tsx',
   output: {
-    filename: "[name].js",
-    path: path.join(__dirname, "/dist"),
+    filename: '[name].js',
+    path: path.join(__dirname, '/dist'),
   },
   resolve: {
-    modules: ["node_modules"],
-    extensions: [".js", ".jsx", ".ts", ".tsx"],
+    modules: ['node_modules'],
+    extensions: ['.js', '.jsx', '.ts', '.tsx'],
   },
   module: {
     rules: [
       {
         test: /\.(ts|tsx)$/,
-        loader: "ts-loader",
+        loader: 'ts-loader',
         options: {
           transpileOnly: isProd ? false : true,
         },
       },
       {
         test: /\.css?$/,
-        use: ["style-loader", "css-loader"],
+        use: ['style-loader', 'css-loader'],
       },
       {
         test: /\.(webp|jpg|png|jpeg)$/,
-        loader: "file-loader",
+        loader: 'file-loader',
         options: {
-          name: "[name].[ext]?[hash]",
+          name: '[name].[ext]?[hash]',
         },
       },
     ],
   },
   plugins: [
     new HtmlWebpackPlugin({
-      template: path.resolve(__dirname, "public", "index.html"),
+      template: path.resolve(__dirname, 'public', 'index.html'),
       hash: true,
     }),
   ],
+  stats: 'errors-only',
   devServer: {
-    contentBase: path.resolve(__dirname, "public"),
-    host: "localhost",
+    static: {
+      directory: path.resolve(__dirname, 'public'),
+    },
     port: PORT,
     open: true,
+    client: {
+      overlay: true,
+    },
     hot: true,
-    compress: true,
+    host: 'localhost',
     historyApiFallback: true,
-    overlay: true,
-    stats: "errors-only",
+    compress: true,
   },
 };
